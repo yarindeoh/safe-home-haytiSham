52eb284385567149cefb8d7484a8e94b5962bf38
Mac <yarindeoh@gmail.com>
2020-06-01 11:07:33 +0300

    add env suppoty
    Signed-off-by: Mac <yarindeoh@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,22 @@ const apiMocker = require('webpack-api-mocker');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const dotenv = require('dotenv');
+const fs = require('fs');
 
-module.exports = (env2, argv) => {
-    // call dotenv and it will return an Object with a parsed key
-    const env = dotenv.config().parsed;
-    // reduce it to a nice object, the same as before
-    const envKeys = Object.keys(env).reduce((prev, next) => {
-        prev[`process.env.${next}`] = JSON.stringify(env[next]);
+module.exports = (env, argv) => {
+    // Get the root path (assuming your webpack config is in the root of your project!)
+    const currentPath = path.join(__dirname);
+    // Create the fallback path (the production .env)
+    const basePath = currentPath + '/.env';
+    // We're concatenating the environment name to our filename to specify the correct env file!
+    const envPath = basePath + '.' + env.ENVIRONMENT;
+    // Check if the file exists, otherwise fall back to the production .env
+    const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+    // Set the path parameter in the dotenv config
+    const fileEnv = dotenv.config({ path: finalPath }).parsed;
+    // reduce it to a nice object, the same as before (but with the variables from the file)
+    const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+        prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
         return prev;
     }, {});
     return {
