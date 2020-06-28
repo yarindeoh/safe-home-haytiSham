75fa00673a07d18b4d8e98cf984660cceb863c16
orly frank <orly@beaconcure.com>
2020-06-28 22:13:23 +0300

    Server implement hard coded tags APIs #48

--- server_api/src/api/api.routes.js
+++ server_api/src/api/api.routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const TagController = require("./tags.controller");
+const tagController = new TagController();
 
 router.get('/status', (req, res) => {
     res.send({ express: 'OK' });
   });
+
+router.get('/getAllTags', tagController.getAllTags.bind(tagController));
+router.get('/getTagsMap', tagController.getTagsMap.bind(tagController));
 
 module.exports = router;
