cdd3353141a3339a9d7a20e3bec477081462ab96
Winteromi <winteromi@gmail.com>
2020-08-13 18:05:08 +0300

    Bug 90 - fix carousel (#91)
    * bug 90 - fix carousel img src
    
    * bug 90 - change carrousel to carousel
    
    * bug 90 - move publicStories to constants
    
    * bug 90 - remove redundant css

--- src/containers/Stories/StoriesView.js
+++ src/containers/Stories/StoriesView.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import { withRoute } from 'services/routing/routerHOC';
-import { StoriesGalleryView } from 'containers/Stories/components/StoriesGallery/StoriesGalleryView';
+import Carousel from 'containers/Stories/components/Carousel/Carousel';
 import { TagsFilter } from 'containers/Stories/components/TagsFilter';
 import { Header } from 'components/Header';
 import { Footer } from 'components/Footer';
 import { useTranslation } from 'react-i18next';
 import HelpButton from 'src/components/HelpButton.js';
 import AccessibilityIcon from 'src/media/icons/accessibility.svg';
-
 
 export const StoriesView = withRoute(props => {
     const { t } = useTranslation();
@@ -21,7 +20,7 @@ export const StoriesView = withRoute(props => {
             <Header />
             <AccessibilityIcon className="AccessibilityButton" />
             <h4 className={'const-text'}>{t('storiesView.header')}</h4>
-            <StoriesGalleryView changeLocationByPath={changeLocationByPath} />
+            <Carousel changeLocationByPath={changeLocationByPath} />
             <button
                 className={'BTN-send-testimony'}
                 onClick={() => props.history.push('addStory')}
