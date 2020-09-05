ae0c295a9fb3d20d3a962213b91ef939d07f7aaa
winteromi <winteromi@gmail.com>
2020-09-05 19:51:25 +0300

    remove accessibility button

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Header } from './Header';
 import Footer from './Footer';
-import AccessibilityIcon from 'src/media/icons/accessibility.svg';
 
 export function Skeleton(props) {
     const { children, isMainHeader } = props;
     return (
         <React.Fragment>
             <Header isMainHeader={isMainHeader} />
-            <AccessibilityIcon className="AccessibilityButton" />
             <div className="Content">{children}</div>
             <Footer />
         </React.Fragment>
