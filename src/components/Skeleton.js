911d76b0a5fe93725cf883416373c236f9b82c15
winteromi <winteromi@gmail.com>
2020-09-21 22:28:01 +0300

    add footer

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Header } from './Header';
 import Footer from './Footer';
 import WideHeader from './WideHeader';
+import WideFooter from './WideFooter';
 import { BREAKPOINT_MAP } from 'src/services/general/breakpoints';
 import { useResize } from 'src/services/general/generalHooks';
 
@@ -9,17 +10,20 @@ export function Skeleton(props) {
     const { children, isMainHeader, id } = props;
     const breakpoint = useResize();
 
+    let header = <Header isMainHeader={isMainHeader} />;
+    let footer = <Footer />;
+    if (breakpoint === BREAKPOINT_MAP.BREAKPOINT_2) {
+        header = <WideHeader />;
+        footer = <WideFooter />;
+    }
+
     return (
         <React.Fragment>
-            {breakpoint === BREAKPOINT_MAP.BREAKPOINT_1 ? (
-                <Header isMainHeader={isMainHeader} />
-            ) : (
-                <WideHeader />
-            )}
+            {header}
             <div id={id} className="Content">
                 {children}
             </div>
-            <Footer />
+            {footer}
         </React.Fragment>
     );
 }
