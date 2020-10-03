65046917445bd98d8394a78707c151f92b70d188
orly frank <orly@beaconcure.com>
2020-10-03 20:48:03 +0100

    GetStoriesByTags to return tags names and not IDs #94

--- server_api/src/api/stories/stories.service.js
+++ server_api/src/api/stories/stories.service.js
@@ -4,23 +4,42 @@ const Story = require("./story.model");
 const ModeratedStrory = require("./moderatedStory.model");
 const Counter = require('./counter.model');
 
+const TagController = require("../tags.controller");
+const tagController = new TagController();
+
 class StorieService {
     constructor() {
     }
 
-    listByTags(tags, page = 1, pageSize = 100, sortField = "createdAt", sortDirection = "DESC"){
+    listByTags(tags, page = 1, pageSize = 100, sortField = "createdAt", sortDirection = "DESC") {
         sortDirection = sortDirection === 'ASC' ? '' : '-';
         sortField = sortDirection + sortField;
-        const query = tags? {tags: {'$in': tags}} : {}; 
+        const query = tags ? { tags: { '$in': tags } } : {};
         return Promise.all([
             ModeratedStrory.countDocuments(query),
             ModeratedStrory.find(query)
                 .sort(sortField)
                 .skip((page - 1) * pageSize).limit(pageSize)
-                .populate('user')
-        ])
-            .then(([count, result]) => ({
-                result, total: count, page: page, pages: Math.ceil(count / pageSize) }));
+                .lean()
+        ]).then(([count, result]) => {
+            // resolve tag id to tag name
+            for(let i=0; i<result.length; i++){
+                let story = result[i];
+                if(story.tags){
+                    story.tagsIds = story.tags;
+                    story.tags = [];
+                    for(let t=0; t<story.tagsIds.length; t++){
+                        let tagName = tagController.tagsMap[story.tagsIds[t]];
+                        if(tagName){
+                            story.tags.push(tagName);
+                        }
+                    }
+                }
+            }
+            return {
+                result, total: count, page: page, pages: Math.ceil(count / pageSize)
+            }
+        });
     }
     
     listStriesToModerate(page = 1, pageSize = 100, sortField = "createdAt", sortDirection = "DESC"){
