0c2233a37e41ac7c99ca0ad73be027823ff1a273
Rootrapee Rattanapipit <rootrapee.r@kbtg.tech>
2020-10-05 15:45:28 +0700

    fix: format date for story-highlight

--- src/containers/Story/components/StoryHighlight.js
+++ src/containers/Story/components/StoryHighlight.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Tag from 'components/Tag';
+import dayjs from 'dayjs';
 
 export const StoryHighlight = ({ story, changeLocationByPath, liStyle }) => {
     const { id, name, createdAt, tags, quote } = story;
     let initials = name && name.split('')[0];
+    const createAtDate = dayjs(createdAt).format('DD.MM.YY');
     const allTags = () => (
         <div className="tags">
             {tags && tags.map((tag, i) => <Tag key={`tag_${i}`} text={tag} />)}
@@ -17,7 +19,7 @@ export const StoryHighlight = ({ story, changeLocationByPath, liStyle }) => {
             onClick={() => changeLocationByPath(id, story)}
         >
             <div className="initials">{initials}</div>
-            <span className="date">{createdAt}</span>
+            <span className="date">{createAtDate}</span>
             <p className="text">{quote}</p>
             {allTags()}
         </li>
