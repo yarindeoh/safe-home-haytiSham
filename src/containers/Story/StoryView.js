d4d202b0a8f80dc99dbdc58c298fbef4190c77fc
winteromi <winteromi@gmail.com>
2020-10-21 21:48:20 +0300

    feat - create StoryInfo comp to share in story pages

--- src/containers/Story/StoryView.js
+++ src/containers/Story/StoryView.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { withRoute } from 'services/routing/routerHOC';
-import { Tags } from './components/Tags';
 import { extractFieldsFromObjOrdered } from 'services/general/generalHelpers';
 import { StoriesList } from 'containers/Stories/components/StoriesList';
-import { useTranslation } from 'react-i18next';
 import Skeleton from 'src/components/Skeleton';
 import HelpButton from 'src/components/HelpButton.js';
-import Content from "../../components/Content";
+import Content from 'src/components/Content';
+import StoryInfo from './components/StoryInfo';
 
 export const StoryView = withRoute(props => {
     const { t } = useTranslation();
@@ -29,21 +29,7 @@ export const StoryView = withRoute(props => {
         <Skeleton>
             <div id={'story-page-container'}>
                 <Content className={'story-page-content'} alignRight={true}>
-                    <div className={'quote'}>
-                        <h1>"{story.quote}"</h1>
-                        <h2>
-                            {`
-                     ${t('storyView.storyOf')}
-                     ${
-                         story.name
-                             ? story.name.split('')[0]
-                             : t('storyView.anonymousTeller')
-                     }
-                     ${story.createdAt}
-                   `}
-                        </h2>
-                        <Tags tags={story.tags} />
-                    </div>
+                    <StoryInfo story={story} />
                     {processedStory &&
                         processedStory.map((item, key) => (
                             <div key={key}>
