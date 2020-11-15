66abf77e99ae59253aec2d25ccf7897e0bf160a2
Yarin Dekel <38718795+yarindeoh@users.noreply.github.com>
2020-11-15 21:08:37 +0200

    Revert "landscape fix, stories column fix (#197)" (#260)
    This reverts commit 3862fa162df3bc0735a60300ad8b4ca666c66b2a.

--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -3,18 +3,27 @@ import { Header } from './Header';
 import { FooterWrapper } from '../containers/Story/components/FooterWrapper';
 import WideHeader from './WideHeader';
 import WideFooter from './WideFooter';
+import { BREAKPOINT_MAP } from 'src/services/general/breakpoints';
+import { useResize } from 'src/services/general/generalHooks';
 
 export function Skeleton(props) {
     const { children, isMainHeader, id } = props;
+    const breakpoint = useResize();
+
+    let header = <Header isMainHeader={isMainHeader} />;
+    let footer = <FooterWrapper />;
+    if (breakpoint === BREAKPOINT_MAP.BREAKPOINT_2) {
+        header = <WideHeader />;
+        footer = <WideFooter />;
+    }
+
     return (
         <React.Fragment>
-            <Header isMainHeader={isMainHeader} />
-            <WideHeader />
+            {header}
             <div id={id} className="Content">
                 {children}
             </div>
-            <FooterWrapper />
-            <WideFooter />
+            {footer}
         </React.Fragment>
     );
 }
