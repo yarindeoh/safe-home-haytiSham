82cc897f647c531214e4f916cc7a7d59bd56f10c
Reut <reut@soficoop.com>
2020-11-17 18:04:52 +0200

    fix: ANALYTICS_TRACKING_ID

--- src/services/analytics/analyticsConstants.js
+++ src/services/analytics/analyticsConstants.js
@@ -1,4 +1,4 @@
-export const ANALYTICS_TRACKING_ID = 'G-624GLBL8ED'
+export const ANALYTICS_TRACKING_ID = 'UA-183066774-1'
 
 export const analyticsEvents = Object.freeze({
   CALLING_HELP: 'CALLING_HELP',
