9d98c22bf25162e1ab002d6d0b6632012a11e13e
Reut <reut@soficoop.com>
2020-11-18 13:10:58 +0200

    feat: adding analytics pageView

--- src/services/analytics/analytics.js
+++ src/services/analytics/analytics.js
@@ -2,7 +2,8 @@ import ReactGA from 'react-ga';
 import {ANALYTICS_TRACKING_ID} from './analyticsConstants'
 
 export const initializeAnalytics = () => {
-  ReactGA.initialize(ANALYTICS_TRACKING_ID);
+  ReactGA.initialize(ANALYTICS_TRACKING_ID, {debug: true});
+  ReactGA.pageview(window.location.pathname + window.location.search);
 }
 
 export const sendEngagementsAnalyticsEvent = (action) => {
