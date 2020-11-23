9d720e4adac0a437815ee6cf346f4b2f8f77726b
rotem12345678 <rotem.shpringer@gmail.com>
2020-11-23 08:46:58 +0200

    change to arrow icon

--- src/containers/Story/components/StoryHighlight.js
+++ src/containers/Story/components/StoryHighlight.js
@@ -3,6 +3,7 @@ import Tag from 'components/Tag';
 import EditImg from 'src/media/icons/Edit.svg';
 import EyeSlashIcon from 'src/media/icons/eye-slash-grey.svg';
 import AvatarIcon from 'src/media/icons/Avatar.svg';
+import ArrowIcon from 'src/media/icons/arrow.svg';
 
 export const StoryHighlight = ({
     story,
@@ -46,7 +47,7 @@ export const StoryHighlight = ({
             {displayAsUnPublish && (
                 <EyeSlashIcon className={'eye-slash-icon-story-highlight'} />
             )}
-            <div className={'arrow-icon'}>{">"}</div>
+            <div className={'arrow-icon'}>{<ArrowIcon/>}</div>
             <div className="initials">
                 <AvatarIcon />
             </div>
