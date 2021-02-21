a2a9e5b4d74779896013fb3f501c906054cd6241
nadavmoreno <nadavmoreno@gmail.com>
2021-02-21 21:17:49 +0200

    fix

--- server_api/src/api/api.routes.js
+++ server_api/src/api/api.routes.js
@@ -1,58 +1,62 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const path = require('path')
-const fs = require('fs');
-const React = require('react')
-const ReactDOMServer = require('react-dom/server');
-const App = require('../../src/App');
+const path = require('path');
 
-const TagController = require("./tags.controller");
+const TagController = require('./tags.controller');
 const tagController = new TagController();
-const StorieController = require("./stories/stories.controller");
-const storieController = new StorieController()
-const UsersController = require("./authentication/users.controller");
-const usersController = new UsersController()
-
-//ssr
-app.get('/', (req, res) => {
-  const app = ReactDOMServer.renderToString(<App />);
-
-  const indexFile = path.resolve('./build/index.html');
-  fs.readFile(indexFile, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Something went wrong:', err);
-      return res.status(500).send('Oops, better luck next time!');
-    }
-
-    return res.send(
-      data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
-    );
-  });
-});
+const StorieController = require('./stories/stories.controller');
+const storieController = new StorieController();
+const UsersController = require('./authentication/users.controller');
+const usersController = new UsersController();
 
 router.get('/status', (req, res) => {
     res.send({ express: 'OK' });
-  });
+});
 
 // anonymous user API
 router.get('/getAllTags', tagController.getAllTags.bind(tagController));
 router.get('/getTagsMap', tagController.getTagsMap.bind(tagController));
-router.get('/getStoriesByTags', storieController.getStoriesByTags.bind(storieController));
+router.get(
+    '/getStoriesByTags',
+    storieController.getStoriesByTags.bind(storieController)
+);
 router.post('/addStory', storieController.addStory.bind(storieController));
 
-const loginGuard =  usersController.validateUserLogIn.bind(usersController);
+const loginGuard = usersController.validateUserLogIn.bind(usersController);
 
 // moderation admin API
 router.post('/login', usersController.login.bind(usersController));
-router.get('/getStortiesForModeration', loginGuard, storieController.getStortiesForModeration.bind(storieController));
-router.get('/getStoryForEdit', loginGuard, storieController.getStoryForEdit.bind(storieController));
-router.post('/addModerateStory', loginGuard, storieController.addModerateStory.bind(storieController));
-router.post('/publishModerateStory', loginGuard, storieController.publishModerateStory.bind(storieController));
-router.get('/getAllModeratedStories', loginGuard, storieController.getAllModeratedStories.bind(storieController));
+router.get(
+    '/getStortiesForModeration',
+    loginGuard,
+    storieController.getStortiesForModeration.bind(storieController)
+);
+router.get(
+    '/getStoryForEdit',
+    loginGuard,
+    storieController.getStoryForEdit.bind(storieController)
+);
+router.post(
+    '/addModerateStory',
+    loginGuard,
+    storieController.addModerateStory.bind(storieController)
+);
+router.post(
+    '/publishModerateStory',
+    loginGuard,
+    storieController.publishModerateStory.bind(storieController)
+);
+router.get(
+    '/getAllModeratedStories',
+    loginGuard,
+    storieController.getAllModeratedStories.bind(storieController)
+);
 
-// send preview image for meta tag 
-router.get('/previewImage', (req, res) => 
-  res.sendFile(path.join(__dirname + '../../../../src/media/assets/preview.png'))
+// send preview image for meta tag
+router.get('/previewImage', (req, res) =>
+    res.sendFile(
+        path.join(__dirname + '../../../../src/media/assets/preview.png')
+    )
 );
 
 module.exports = router;
